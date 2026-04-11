7c665499a1850e3705975e94c14dcf585d1c8b7e
Jemin <jaymindesai237@gmail.com>
2026-04-11 11:47:02 +0530

    feat(server): change module type to ES module and update express import syntax

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import type { Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { GoogleGenerativeAI } from "@google/generative-ai";
